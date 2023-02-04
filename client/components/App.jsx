514a67e410ba9e857470a2cda021d1dd9c4fe48f
Kevin Hendrix <k.hendrix1204@gmail.com>
2023-02-04 17:41:02 -0500

    Add logic to display data from backend in table component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FeatureContainer from '../containers/FeaturePageContainer';
+import Login from './Login';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" exact element={<App />} />
-          <Route path="/feature" exact element={<FeatureContainer />} />
-        </Routes>
-      </Router>
-      ,
-    </>
+
+    <Router>
+      <Routes>
+        <Route path="/" exact element={<Login />} />
+        <Route path="/feature" exact element={<FeatureContainer />} />
+      </Routes>
+    </Router>
 
   );
 }
