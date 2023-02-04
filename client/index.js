7a94203ac510f82770081e347aacf2249b88ccef
jm-roman <johnmroman33@gmail.com>
2023-02-03 20:40:04 -0500

    initial boilerplate

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './components/App.jsx';
+
+// import styles from './scss/application.scss';
+
+const domNode = document.getElementById('app');
+const root = createRoot(domNode);
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+);
