031da522b979409d30b3e366e1fb97581b149464
Kevin Hendrix <k.hendrix1204@gmail.com>
2023-02-04 15:21:23 -0500

    Initialize components for login page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FeatureContainer from '../containers/FeaturePageContainer';
 
 function App() {
   return (
-    <FeatureContainer />
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" exact element={<App />} />
+          <Route path="/feature" exact element={<FeatureContainer />} />
+        </Routes>
+      </Router>
+      ,
+    </>
+
   );
 }
 
--- client/index.js
+++ client/index.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './components/App.jsx';
+
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import App from './components/App';
 
 // import styles from './scss/application.scss';
 
-const domNode = document.getElementById('app');
-const root = createRoot(domNode);
-root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
