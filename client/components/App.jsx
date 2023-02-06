bdbbf68d78694f9cfef09b93ed55662f5ed95479
Kevin Hendrix <k.hendrix1204@gmail.com>
2023-02-06 14:58:35 -0500

    Initialize react structure for login page

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FeatureContainer from '../containers/FeaturePageContainer';
 import Login from './Login';
 
 function App() {
+  const [globalUser, setGlobalUser] = useState('');
   return (
 
     <Router>
       <Routes>
-        <Route path="/" exact element={<Login />} />
+        <Route path="/" exact element={<Login setGlobalUser={setGlobalUser} globalUser={globalUser}/>} />
         <Route path="/feature" exact element={<FeatureContainer />} />
       </Routes>
     </Router>
