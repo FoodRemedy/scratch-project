655d2f9eb3be2ee3a572e16117e3cf82dd0bd033
Sheng Li <shengli@Shengs-MBP.attlocal.net>
2023-02-06 12:16:29 -0800

    mimum change made

--- client/index.js
+++ client/index.js
@@ -4,6 +4,6 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './components/App';
 
-// import styles from './scss/application.scss';
+import styles from './scss/application.scss';
 
 ReactDOM.createRoot(document.getElementById('app')).render(<App />);
