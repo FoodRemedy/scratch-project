81002be8aeac868b58be011c2a393e70eeec66e0
Julien Devlin <juliendevlin@gmail.com>
2023-02-07 18:22:30 -0500

    refactor createUser method from userController

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,26 +1,56 @@
 const User = require('../models/userModel');
 const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
 const userController = {};
 
+// Writes a new a user to the database
 userController.createUser = async (req, res, next) => {
   const { username, password } = req.body;
 
-  try {
-    //check first to see if user is already created
-    const found = await User.findOne({ username });
-    if (found) {
-      console.log('yes user is created');
-      throw new Error('username is already in use');
-    }
-    const newUser = await new User({ username, password });
-    await newUser.save();
-    res.locals.user = newUser;
-    return next();
-  } catch (error) {
+  // Username type validation - must be type String between 6 and 30 characters inclusive
+  if (typeof username !== 'string' || username.length < 6 || username.length > 30) {
     return next({
-      log: 'Error in userController.createUser middleware function',
-      status: 500,
-      message: { err: error.message },
+      log: 'ERROR - userController.createUser: request body contains invalid username.',
+      status: 400,
+      message: { err: 'Username must be between 6 and 30 characters in length.' },
+    });
+  }
+
+  // Password type validation
+  if (typeof password !== 'string' || password.length < 8) {
+    return next({
+      log: 'ERROR - userController.createUser: request body contains invalid password.',
+      status: 400,
+      message: { err: 'Password must have a minimum of 8 characters.' },
+    });
+  }
+
+  try {
+    // Check if username already exists
+    const usernameMatch = await User.findOne({ username });
+    if (usernameMatch !== null) {
+      return next({
+        log: 'ERROR - userController.createUser: request body contains username that is already in use.',
+        status: 400,
+        message: { err: 'Username is already in use.' },
+      });
+    }
+
+    // Create hashed password
+    const hashedPassword = await bcrypt.hash(password, SALT_WORK_FACTOR);
+
+    // Write new user to database
+    const user = await User.create({ username, hashedPassword });
+    res.locals.user = user;
+    
+    return next();
+  } 
+  catch (err) {
+    return next({
+      log: `ERROR - userController.createUser: ${err}`,
+      status: 400,
+      message: { err: 'Failed to create user. Check server log for details.' },
     });
   }
 };
