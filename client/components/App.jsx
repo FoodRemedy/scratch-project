d164815eaae0754bf2d830f60cdb42a175776967
Zahara Aviv <zahara.aviv@gmail.com>
2023-02-08 00:13:06 -0500

    mobile friendly styling

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,20 +1,39 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useNavigate,
+} from 'react-router-dom';
 import FeatureContainer from '../containers/FeaturePageContainer';
 import Login from './Login';
-
 
 function App() {
   const [globalUser, setGlobalUser] = useState('');
   return (
-    
     <Router>
       <Routes>
-        <Route path="/" exact element={<Login setGlobalUser={setGlobalUser} globalUser={globalUser}/>} />
-        <Route path="/feature" exact element={ globalUser ? <FeatureContainer globalUser={globalUser} setGlobalUser={setGlobalUser}/> : null} />
+        <Route
+          path='/'
+          exact
+          element={
+            <Login setGlobalUser={setGlobalUser} globalUser={globalUser} />
+          }
+        />
+        <Route
+          path='/feature'
+          exact
+          element={
+            globalUser ? (
+              <FeatureContainer
+                globalUser={globalUser}
+                setGlobalUser={setGlobalUser}
+              />
+            ) : null
+          }
+        />
       </Routes>
     </Router>
-
   );
 }
 
